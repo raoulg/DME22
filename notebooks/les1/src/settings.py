cc92309f56a55c1192cf2a643726a85d9a8016b6
Jan-Willem Lankhaar <jan-willem.lankhaar@hu.nl>
2022-09-19 18:14:16 +0000

    Comments for logger configuration

--- notebooks/les1/src/settings.py
+++ notebooks/les1/src/settings.py
@@ -13,20 +13,22 @@ class Settings(BaseSettings):
 
     namecol: str = "name"
 
-settings = Settings()
+settings = Settings()       # This object should be imported in other modules.
 
+# Configure the 'sinks' of the logger (i.e. the outputs to where log information
+# is written to).
 logger.add(
-    sink=settings.logdir / "log_{time:YYYYMMDD}.log",
-    rotation="00:00",
+    sink=settings.logdir / "log_{time:YYYYMMDD}.log",   # A log file with a date stamp.
+    rotation="00:00",       # A new log file is created at midnight.
     enqueue=True
 )
 logger.add(
-    sink=sys.stdout, 
-    colorize=True, 
+    sink=sys.stdout,                    # The screen (standard output 'channel').
+    colorize=True,                      # Add colors to the messages.
     format="<green>{time}</green> <level>{message}</level>"
 )
 logger.add(
-    sink=sys.stderr,
-    colorize=True,
+    sink=sys.stderr,                    # The screen (standard error 'channel').
+    colorize=True,                      
     format="<green>{time}</green> <level>{message}</level>"
 )
