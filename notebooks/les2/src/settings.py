2f94638de6eb38a6f34882c03da5341ae3878698
Raoul Grouls <raoul.grouls@businessdecision.nl>
2022-09-19 12:20:17 +0200

    src

--- notebooks/les2/src/settings.py
+++ notebooks/les2/src/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import Dict, List, Tuple
 
 from pydantic import BaseModel
 
@@ -6,4 +7,13 @@ from pydantic import BaseModel
 class Settings(BaseModel):
     datadir: Path = Path("data/raw")
     outputdir: Path = Path("data/processed")
-    namecol: str = "name"
+    imagedir: Path = Path("reports/img")
+    scatter: Dict = {"x": "sepal_length", "y": "sepal_width"}
+    growth: Dict = {"title" : "Average growth (width/length)",
+    "xlab" : "width/length"}
+    speciesorder: List[str] = ["virginica", "versicolor", "setosa"]
+    palette: str = "Set1"
+    alpha: float = 0.7
+    target: str = "species"
+    title: str = "Iris dataset"
+    figsize: Tuple = (17,8)
