e4a9942c8a8194d3024f4ad3f5bff76fe7c6512b
Raoul Grouls <Raoul.Grouls@han.nl>
2023-09-07 12:40:25 +0000

    pydantic basemodel

--- notebooks/les1/src/settings.py
+++ notebooks/les1/src/settings.py
@@ -1,11 +1,10 @@
-import sys
 from pathlib import Path
 
 from loguru import logger
-from pydantic import BaseSettings
+from pydantic import BaseModel
 
 
-class Settings(BaseSettings):
+class Settings(BaseModel):
     basedir: Path = Path.cwd()
     datadir: Path = Path("data/raw")
     outputdir: Path = Path("data/processed")
@@ -15,21 +14,4 @@ class Settings(BaseSettings):
 
 
 settings = Settings()  # This object should be imported in other modules.
-
-# Configure the 'sinks' of the logger (i.e. the outputs to where log information
-# is written to).
-logger.add(
-    sink=settings.logdir / "log_{time:YYYYMMDD}.log",  # A log file with a date stamp.
-    rotation="00:00",  # A new log file is created at midnight.
-    enqueue=True,
-)
-logger.add(
-    sink=sys.stdout,  # The screen (standard output 'channel').
-    colorize=True,  # Add colors to the messages.
-    format="<green>{time}</green> <level>{message}</level>",
-)
-logger.add(
-    sink=sys.stderr,  # The screen (standard error 'channel').
-    colorize=True,
-    format="<green>{time}</green> <level>{message}</level>",
-)
+logger.add("logfile.log")
