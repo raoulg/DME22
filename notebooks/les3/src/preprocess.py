25be158cab2010cef2a2936d079d0d4a879629d7
Raoul Grouls <Raoul.Grouls@han.nl>
2023-09-26 09:32:01 +0200

    [les3] some comments

--- notebooks/les3/src/preprocess.py
+++ notebooks/les3/src/preprocess.py
@@ -2,17 +2,26 @@ import polars as pl
 
 
 def prepare_floats(filename, modelsettings) -> pl.DataFrame:
+    # the target column is the column we want to predict
     target = modelsettings.target
 
     df = pl.read_parquet(filename)
     p = (
+        # with select we create a context to work on.
+        # we want just the float columns and the target column
         df.select(
             [
                 pl.col(pl.Float64),  # select float
                 pl.col(target),  # and the target column
             ]
         )
+        # we melt this selection into a long format
+        # the target column is the id column
         .melt(id_vars=target)
+        # we create a new context:
+        # 1. all cols
+        # 2. the mean of the value column, for every variable, renamed to mean/var
+        # 3. the std of the value column, for every variable, renamed to std/var
         .select(
             [  # melt on target
                 pl.col("*"),
@@ -23,6 +32,7 @@ def prepare_floats(filename, modelsettings) -> pl.DataFrame:
                 pl.col("value").std().over("variable").alias("std/var"),  # and std
             ]
         )
+        # now we have these new columns, we can calculate a normalized value
         .with_columns(
             [
                 pl.struct(["value", "mean/var", "std/var"])
