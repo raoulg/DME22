d5046f5d08d952a6f26b630dd4b61df7dfd371e8
Raoul Grouls <Raoul.Grouls@han.nl>
2023-09-26 10:03:00 +0200

    [les4] clean up knmi

--- notebooks/les4/src/example.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import streamlit as st
-
-df = pd.DataFrame({"first column": [1, 2, 3, 4], "second column": [10, 20, 30, 40]})
-st.write(df)
-
-option = st.selectbox("Which number do you like best?", df["first column"])
-
-st.write("You selected: ", option)
